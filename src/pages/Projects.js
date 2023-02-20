37e5518d6100eef2617d541785ed36b56d4ad005
Your NameNazar <nazarkruk@icloud.com>
2023-02-20 14:17:55 -0500

    Projects page build

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,67 @@
-import React from "react";
+import axios from "axios";
+import React, { Suspense, useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import Layout from "../components/Layout";
-const Projects = () => {
+import Pagination from "../components/Pagination";
+import PortfoliosView from "../components/PortfoliosView";
+import SectionTitle from "../components/SectionTitle";
+import Spinner from "../components/Spinner";
+
+function Projects() {
+  const [portfolios, setPortfoios] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [portfoliosPerPage] = useState(9);
+
+  useEffect(() => {
+    let mounted = true;
+    axios.get("/api/portfolios").then((response) => {
+      if (mounted) {
+        setProjects(response.data);
+      }
+    });
+    return () => (mounted = false);
+  }, []);
+
+  const indexOfLastPortfolios = currentPage * portfoliosPerPage;
+  const indexOfFirstPortfolios = indexOfLastPortfolios - portfoliosPerPage;
+  const currentPortfolios = portfolios.slice(
+    indexOfFirstPortfolios,
+    indexOfLastPortfolios
+  );
+
+  const paginate = (e, pageNumber) => {
+    e.preventDefault();
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <Layout>
-      <h1>Project Page</h1>
+      <Helmet>
+        <title>Portfolios - Chester React Personal Portfolio Template</title>
+        <meta
+          name="description"
+          content="Chester React Personal Portfolio Template Portfolios Page"
+        />
+      </Helmet>
+      <Suspense fallback={<Spinner />}>
+        <div className="mi-about mi-section mi-padding-top mi-padding-bottom">
+          <div className="container">
+            <SectionTitle title="Portfolios" />
+            {<PortfoliosView portfolios={currentPortfolios} />}
+            {!(portfolios.length > portfoliosPerPage) ? null : (
+              <Pagination
+                className="mt-50"
+                itemsPerPage={portfoliosPerPage}
+                totalItems={portfolios.length}
+                paginate={paginate}
+                currentPage={currentPage}
+              />
+            )}
+          </div>
+        </div>
+      </Suspense>
     </Layout>
   );
-};
+}
 
 export default Projects;
